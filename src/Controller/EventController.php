e8d7fd57dcfd3fe33c1ced25acc711edd41bde29
mbsenvoillage <momboisse.yvn@gmail.com>
2020-09-24 11:40:36 +0200

    event signup/signout/cancel

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -8,6 +8,7 @@ use App\Entity\Place;
 use App\Entity\User;
 use App\Form\EventType;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -63,5 +64,75 @@ class EventController extends AbstractController
         $idPlaces = $placeRepo->findAllByCity($idCity);
 
         return new JsonResponse(json_encode($idPlaces));
+    }
+
+    /**
+     * @Route("/signup-sortie-{id}", name="signup")
+     */
+    public function eventSignup(Event $event)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userA = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
+
+        $now = new \DateTime("now");
+
+        $now->add(new \DateInterval('PT2H'));
+
+
+        if($event->getClosingDate() < $now)
+        {
+            $event->addAttendee($userA);
+            $em->flush();
+            $this->addFlash('success', "Votre inscription a été prise en compte");
+        }
+        else {
+            $this->addFlash('error', "Vous ne pouvez pas vous inscrire, la date limite d'inscription est dépassée");
+        }
+
+        $response = $this->forward('App\Controller\MainController::home');
+        return $response;
+
+
+    }
+
+    /**
+     * @Route("/signout-sortie-{id}", name="signout")
+     */
+    public function eventSignout(Event $event)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userA = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
+
+        $event->removeAttendee($userA);
+        $em->flush();
+
+        $response = $this->forward('App\Controller\MainController::home');
+        return $response;
+    }
+
+    /**
+     * @Route("/annuler-sortie-{id}", name="remove")
+     */
+    public function removeEvent(Event $event)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userA = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
+
+        $organiserId = $event->getOrganizer()->getId();
+        $userId = $userA->getId();
+
+        if ($organiserId == $userId)
+        {
+
+            $em->remove($event);
+            $em->flush();
+            $this->addFlash('error', "La sortie a bien été supprimée");
+        }
+        else {
+            $this->addFlash('error', "Vous ne pouvez pas supprimer une sortie dont vous n'êtes pas l'organisateur");
+        }
+
+        $response = $this->forward('App\Controller\MainController::home');
+        return $response;
     }
 }
