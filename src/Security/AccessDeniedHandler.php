f445592d8346dcd88823ed04adbacafc3154d851
mbsenvoillage <momboisse.yvn@gmail.com>
2020-10-01 15:13:08 +0200

    403 style up unicorn style maggle

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -22,7 +22,35 @@ class AccessDeniedHandler implements \Symfony\Component\Security\Http\Authorizat
      */
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
-        $content = "<h1>Oops, access denied ... </h1><a href='/sortir/public/'>Retour à l'accueil</a>";
+        $content = "
+<!DOCTYPE html>
+<html lang='en'>
+<style>
+a:visited {
+color: black;
+}
+button {
+ text-decoration: none;
+  border: 2px solid #F5EEEB;
+  padding: 8px 15px;
+  border-radius: 10px;
+  background-color: #FFF7BF;
+  color: #709FD3;
+  font-weight: bold; 
+}
+h1 {
+    color: #FB999A;
+}
+html {
+background-color: #A6DAF5;
+}
+</style>
+<div style='width: 80%; margin: auto; padding: 10px; text-align: center'>
+<img src='/sortir/public/icons/unicorn.png' alt='surprised unicorn' style='margin-left: 3em'>
+<h1>Oops, access denied ... </h1>
+<a href='/sortir/public/' style='text-decoration: none'><button>Retour à l'accueil</button></a>
+</div>
+</html>";
         return new Response($content, 403);
     }
 }
